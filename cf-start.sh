f4c0c67086ea649583c2506b0f0c88323050e89a
Deepu Roy <Deepu.Roy@cognizant.com>
2018-05-15 20:59:00 +0530

    Displaying plan name and created start/stop linux scripts

---
+++ cf-start.sh
@@ -0,0 +1,7 @@
+cf start eureka-v1a &&
+cf start plan-v1a &&
+cf start fund-v1a &&
+cf start participant-management-v1a &&
+cf start accounts-v1a &&
+cf start account-contrib-v1a &&
+cf start gateway-v1a
---
+++ cf-stop.sh
@@ -0,0 +1,10 @@
+cf stop gateway-v1a &&
+cf stop plan-v1a &&
+cf stop participant-management-v1a &&
+cf stop accounts-v1a &&
+cf stop account-contrib-v1a &&
+cf stop eureka-v1a &&
+cf stop fund-v1a
+
+
+
